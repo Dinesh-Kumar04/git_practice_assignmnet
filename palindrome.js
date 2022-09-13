1c15ad9b8fe2db9df2b3c4cfbadf30ebd2bef59f
netsurfer <netsurfing753@gmail.com>
2022-09-13 21:32:56 +0530

    Fixed the string problem

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,7 @@
 let str = 'madam';
 let reverse = '';
 for(let i = str.length-1; i >= 0; i--){
-    reverse = reverse + i
+    reverse = reverse + str[i]
 }
 if(str===reverse){
     console.log('Yes')
